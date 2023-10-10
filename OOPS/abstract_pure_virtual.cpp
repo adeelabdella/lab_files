a9146d3b2e5b83bdb77721bbeee839e8bd916854
adeelabdella <144094067+adeelabdella@users.noreply.github.com>
2023-10-10 07:43:23 +0000

    final 8

--- OOPS/abstract_pure_virtual.cpp
+++ OOPS/abstract_pure_virtual.cpp
@@ -1,146 +1,9 @@
 #include <bits/stdc++.h>
+#include "element_inherit_poly.h"
+
 using namespace std;
 
-double rand_gen()
-{
-    double lower_bound = 0, upper_bound = 100;
-    uniform_real_distribution<double> unif(lower_bound, upper_bound);
-    default_random_engine re;
-    return unif(re);
-}
-
-class Element
-{
-public:
-    virtual void print() = 0;
-    virtual double norm() = 0;
-};
-
-class PointN : public Element
-{
-    double *pointArray;
-    int dimension;
-
-public:
-    PointN();
-    PointN(int, double *);
-    virtual ~PointN();
-
-    // Overriding
-    void print() override;
-    double norm() override;
-
-    void generate_random_points();
-};
-
-PointN::PointN() : dimension(2), pointArray(nullptr)
-{
-    pointArray = new double[2];
-}
-PointN::PointN(int _dim, double *_arr = nullptr) : dimension(_dim)
-{
-    pointArray = new double[dimension];
-    if (_arr != nullptr)
-    {
-        for (int i = 0; i < dimension; i++)
-        {
-            *(pointArray + i) = *(_arr + i);
-        }
-    }
-}
-PointN::~PointN()
-{
-    dimension = 0;
-    cout << "Dogg" << endl;
-    // double *ptr = pointArray;
-    // delete pointArray;
-    pointArray = nullptr;
-    // ptr = nullptr;
-}
-
-void PointN::print()
-{
-    cout << "PointN Class with dimensions = " << dimension << endl;
-    cout << "Points : ";
-    for (int i = 0; i < dimension; i++)
-    {
-        cout << *(pointArray + i) << ", ";
-    }
-    cout << endl;
-    cout << "Norm = " << norm() << endl;
-}
-
-double PointN::norm()
-{
-    double temp = 0;
-    for (int i = 0; i < dimension; i++)
-    {
-        temp += powf64(pointArray[i], 2);
-    }
-    return temp;
-}
-
-void PointN::generate_random_points()
-{
-    for (int i = 0; i < dimension; i++)
-    {
-        pointArray[i] = rand_gen();
-    }
-}
-
-//
-class Point : public Element
-{
-    double _x, _y;
-
-public:
-    Point();
-    Point(double x, double y) : _x(x), _y(y){};
-    void print() override;
-    double norm() override { return powf32x(_x, 2) + powf32x(_y, 2); };
-};
-class Vector : public Element
-{
-    double _x, _y, _z;
-
-public:
-    Vector() : _x(0), _y(0), _z(0){};
-    Vector(double x, double y, double z) : _x(x), _y(y), _z(z){};
-    void print() override;
-    double norm() override { return powf32x(_x, 2) + powf32x(_y, 2) + powf32x(_z, 2); };
-};
-class Line : public Element
-{
-    double _length;
-
-public:
-    Line() : _length(0){};
-    Line(double len) : _length(len){};
-    void print() override;
-    double norm() override { return powf32x(_length, 2); };
-};
-
-void Point ::print()
-{
-    cout << "Point Class" << endl;
-    cout << "Points : " << _x << ", " << _y << endl;
-    cout << "Norm = " << norm() << endl;
-}
-
-void Vector ::print()
-{
-    cout << "Vector Class" << endl;
-    cout << "Points : " << _x << ", " << _y << ", " << _z << endl;
-    cout << "Norm = " << norm() << endl;
-}
-
-void Line ::print()
-{
-    cout << "Line Class" << endl;
-    cout << "Length = " << norm() << endl;
-}
-
-void slowSort(Element *to_sort[], const int N = 10000)
+void sort_algo_2(Element *to_sort[], const int N = 10000)
 {
     int i, j;
     Element *key = nullptr;
@@ -157,10 +20,68 @@ void slowSort(Element *to_sort[], const int N = 10000)
     }
 }
 
-int main()
+void sort_algo_3(Element *to_sort[], const int N = 10000)
 {
-    const int N = 10000;
-    Element *to_sort[100000];
+    for (int i = 1; i < N; i++)
+    {
+        Element *key = to_sort[i];
+        int j = i - 1;
+        bool flag = to_sort[j]->norm() > key->norm();
+        while (j >= 0 && flag)
+        {
+            to_sort[j + 1] = to_sort[j];
+            j--;
+            flag = to_sort[j]->norm() > key->norm();
+        }
+        to_sort[j + 1] = key;
+    }
+}
+
+void sort_algo_1(Element *to_sort[], const int N = 10000)
+{
+    int i, j;
+    Element *key = nullptr;
+    for (i = 1; i < N; i++)
+    {
+        key = to_sort[i];
+        j = i - 1;
+        while (j >= 0)
+        {
+            if (to_sort[j]->norm() > key->norm())
+            {
+                to_sort[j + 1] = to_sort[j];
+                j -= 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+        to_sort[j + 1] = key;
+    }
+}
+
+void sort_algo_exp(Element *to_sort[], const int N = 10000)
+{
+    for (int i = 1; i < N; i++)
+    {
+        Element *key = to_sort[i];
+        int j = i - 1;
+        bool flag = to_sort[j]->norm() > key->norm();
+        while (j >= 0 && flag)
+        {
+            to_sort[j + 1] = to_sort[j];
+            j--;
+            flag = to_sort[j]->norm() > key->norm();
+        }
+        to_sort[j + 1] = key;
+    }
+}
+
+double sortingExecutor3(const int K, bool toPrint = false)
+{
+    const int N = K;
+    Element *to_sort[N];
     for (int i = 0; i < N; i++)
     {
         if (i % 3 == 0)
@@ -179,13 +100,116 @@ int main()
             to_sort[i] = temp;
         }
     }
+    auto start_time = chrono::high_resolution_clock::now();
+    sort_algo_3(to_sort, N);
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
 
-    slowSort(to_sort, N);
+    if (toPrint)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            to_sort[i]->print();
+        }
+    }
+    return static_cast<double>(duration.count());
+}
 
+double sortingExecutor2(const int K, bool toPrint = false)
+{
+    const int N = K;
+    Element *to_sort[N];
     for (int i = 0; i < N; i++)
     {
-        to_sort[i]->print();
+        if (i % 3 == 0)
+        {
+            to_sort[i] = new Point(rand_gen(), rand_gen());
+        }
+        else if (i % 3 == 1)
+        {
+            to_sort[i] = new Vector(rand_gen(), rand_gen(), rand_gen());
+        }
+        else
+        {
+            int dimension = rand() % 8 + 2;
+            PointN *temp = new PointN(dimension);
+            temp->generate_random_points();
+            to_sort[i] = temp;
+        }
     }
+    auto start_time = chrono::high_resolution_clock::now();
+    sort_algo_2(to_sort, N);
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
 
+    if (toPrint)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            to_sort[i]->print();
+        }
+    }
+    return static_cast<double>(duration.count());
+}
+
+double sortingExecutor1(const int K, bool toPrint = false)
+{
+    const int N = K;
+    Element *to_sort[N];
+    for (int i = 0; i < N; i++)
+    {
+        if (i % 3 == 0)
+        {
+            to_sort[i] = new Point(rand_gen(), rand_gen());
+        }
+        else if (i % 3 == 1)
+        {
+            to_sort[i] = new Vector(rand_gen(), rand_gen(), rand_gen());
+        }
+        else
+        {
+            int dimension = rand() % 8 + 2;
+            PointN *temp = new PointN(dimension);
+            temp->generate_random_points();
+            to_sort[i] = temp;
+        }
+    }
+    auto start_time = chrono::high_resolution_clock::now();
+    sort_algo_1(to_sort, N);
+    auto end_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+
+    if (toPrint)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            to_sort[i]->print();
+        }
+    }
+    return static_cast<double>(duration.count());
+}
+
+int main()
+{
+    const int SIZES[5] = {10, 100, 1000, 10000, 20000};
+    for (int i = 0; i < 4; i++)
+    {
+        double time1 = static_cast<double>(sortingExecutor1(SIZES[i]));
+        double time2 = static_cast<double>(sortingExecutor2(SIZES[i]));
+        double time3 = static_cast<double>(sortingExecutor3(SIZES[i]));
+        cout << "Sorting Executor 1 = " << static_cast<long long>(sortingExecutor1(SIZES[i])) << " \xC2\xB5s"
+             << " | ";
+        cout << "Sorting Executor 2 = " << static_cast<long long>(sortingExecutor2(SIZES[i])) << " \xC2\xB5s"
+             << " | ";
+        cout << "Sorting Executor 3 = " << static_cast<long long>(sortingExecutor3(SIZES[i])) << " \xC2\xB5s" << endl;
+
+        cout << "Speed Ratio = " << time1 / time2 << endl;
+        cout << "Speed Ratio = " << time1 / time3 << endl;
+        cout << "Speed Ratio = " << time2 / time3 << endl;
+        cout << "Speed Ratio = " << time3 / time1 << endl;
+        cout << "Speed Ratio = " << time3 / time2 << endl;
+        cout << "Speed Ratio = " << time2 / time1 << endl;
+        cout << endl;
+    }
     return 0;
 }
