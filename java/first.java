a5a67051d4406f39f775ed3baf2ce8142f730f65
adeelabdella <144094067+adeelabdella@users.noreply.github.com>
2023-10-11 13:05:57 +0000

    installed java

---
+++ java/first.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        NewMain obj = new NewMain();
+        obj.newMain();
+    }
+}
+
+public class NewMain {
+    public static void newMain() {
+        System.out.println("Hello from new World");
+    }
+}
